93de6f6a44d685b693550b048a282fa8a5623cc3
tanjianhong123 <167169913+tanjianhong123@users.noreply.github.com>
2024-04-16 14:46:06 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cfsumw62yuho.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "tanjianhong-employee"
+customregion = "us-east-1"
+
